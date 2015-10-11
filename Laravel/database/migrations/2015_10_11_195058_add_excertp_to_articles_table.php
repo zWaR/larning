f01ac69d1a74c4d8912ee1df55b5c3ac540fc2ca
zWaR <zwar_the_one@hotmail.com>
2015-10-11 21:57:47 +0200

    Migrations

---
+++ Laravel/database/migrations/2015_10_11_195058_add_excertp_to_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddExcertpToArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table)
+        {
+            $table->text('excerpt')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table)
+        {
+            $table->dropColumn('excerpt');
+        });
+    }
+}
