f9ed355f31fc9d1b9ba9f06636093495b2dea373
vagrant <vagrant@homestead>
2015-10-12 09:20:34 +0000

    Eloquent 101

---
+++ Laravel/app/Tasks.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tasks extends Model
+{
+    protected $fillable = [
+        'task',
+        'task_description'
+    ];
+}
